0e33e162fcde3e55af684902e9ef9936a0203d23
Magnus Nilsson <regnhundar@gmail.com>
2024-10-06 01:47:23 +0200

    Layout för aboutsection börjar komma fram. Måste fixa bättre selektorer...

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,8 @@ import "./header.css";
 const Header: React.FC = () => {
     return (
         <header className="header">
-            <h1 className="header__title">
-                <span className="header__title--top">Magnus</span> <span className="header__title--bottom">DEV</span>
-            </h1>
+            <h2 className="header__title header__title--top">Magnus</h2>
+            <h3 className="header__title header__title--bottom">DEV</h3>
             <figure className="header__blob-container">
                 <img className="header__image" src="assets/images/artist.svg" alt="Man with a moustache holding the colors the page is built with." />
             </figure>

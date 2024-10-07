628597011bf01c32abd97794dbdac498df0441a2
Magnus Nilsson <regnhundar@gmail.com>
2024-10-07 22:51:18 +0200

    Första idé på layout klar. Behöver justeras med sizing osv

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 function App() {
     return (
         <>
+            <Header />
+            <Navigation />
             <Routes>
                 <Route path="/" element={<HomePage />} />
             </Routes>
+            <Footer />
         </>
     );
 }
